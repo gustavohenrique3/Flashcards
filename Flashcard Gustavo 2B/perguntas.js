079193af8a7b0b78135c390ffc9d67e5836b3f27
gustavohenrique3 <parreira.gustavo@escola.pr.gov.br>
2025-06-05 13:49:34 -0300

    Add files via upload

---
+++ Flashcard Gustavo 2B/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Automobilismo',
+    'Em que ano Lewis Hamilton estreou na Fórmula 1?',
+    'Lewis Hamilton fez sua estreia na F1 em 2007 pela equipe da McLaren.'
+)
+
+criaCartao(
+    'Automobilismo',
+    'Porqual equipe Lewis Hamilton corre atualmente?',
+    'Atualmente, Hamilton corre pela Ferrari.'
+)
+
+criaCartao(
+    'Automobilismo',
+    'Quantos campeonatos mundiais Lewis Hamilton conquistou?',
+    'Ele conquistou 7 campeonatos mundiais.'
+)
+
+criaCartao(
+    'Automobilismo',
+    'Qual é o circuito onde Hamilton possui mais vitórias na carreira?',
+    'Silverstone com 9 vitórias.'
+)
